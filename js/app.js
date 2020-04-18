619d50d4723f58a202fa411eb47fb1c5ea00877e
Katherine Langley <promoteglobal@gmail.com>
2020-04-18 15:39:20 +0800

    cleaned code spacing

--- js/app.js
+++ js/app.js
@@ -42,31 +42,40 @@ function mix () {
   var arr = Array.prototype.slice.call(listItems);
   let shuffledArr = shuffle(arr);
   cardBoard.innerHTML = '';
-    for(let i = 0; i < 16; i++){
+  
+  for(let i = 0; i < 16; i++){
       cardBoard.appendChild(shuffledArr[i]);
-    }
-   let clearmatch = document.querySelectorAll(".card.match");
-   for (var i = 0; i < clearmatch.length; i++) {
+  }
+   
+  let clearmatch = document.querySelectorAll(".card.match");
+   
+  for (var i = 0; i < clearmatch.length; i++) {
     clearmatch[i].classList.toggle("match");
   }
-    let clearNonMatch = document.querySelectorAll(".card.open.show");
-    for (var i = 0; i < clearNonMatch.length; i++) {
+    
+  let clearNonMatch = document.querySelectorAll(".card.open.show");
+    
+  for (var i = 0; i < clearNonMatch.length; i++) {
       clearNonMatch[i].classList.toggle("open");
       clearNonMatch[i].classList.toggle("show");
   }
+  
   document.querySelector(".timer").innerHTML = 0;
+  
   if (constTimer === false) {
     constTimer = setInterval(myTimer ,1000);
   }
+  
   let starsList = document.querySelector(".stars");
   starsList.innerHTML = '';
+  
   for (let i = 0; i < 5; i++){
     const li = document.createElement('li');
     li.innerHTML = '<i class="fa fa-star"></i>';
     starsList.appendChild(li);
-   }
-   matchCount = 0;
+  }
   
+  matchCount = 0; 
 }
 
 reset.addEventListener('click', mix);
@@ -75,30 +84,30 @@ reset.addEventListener('click', mix);
 
 let card1 = cardBoard.addEventListener('click', function frstCrd (event) {
   if (event.target.classList.contains("card")){
-  let counter = document.querySelector('.moves').innerHTML;
+    let counter = document.querySelector('.moves').innerHTML;
+    
     if(counter % 2 === 0){
       card1 = event.target.firstElementChild.classList[1];
-      console.log("card 1 was clicked");
     }
   }
 });
 
 let card2 = cardBoard.addEventListener('click', function scndCrd (event) {
   if (event.target.classList.contains("card")){
-  let counter = document.querySelector('.moves').innerHTML;
+    let counter = document.querySelector('.moves').innerHTML;
+    
     if (counter % 2 !== 0){
       card2 = event.target.firstElementChild.classList[1];
-      console.log("card 2 was clicked");
     }
   }
 });
 
 let card1El = cardBoard.addEventListener('click', function frstCrdEl (event) {
   if (event.target.classList.contains("card")){
-  let counter = document.querySelector('.moves').innerHTML;
+    let counter = document.querySelector('.moves').innerHTML;
+    
     if(counter % 2 === 0){
       card1El = event.target;
-      console.log("cardEl 1 was clicked");
     }
   }
 });
@@ -107,103 +116,97 @@ let card1El = cardBoard.addEventListener('click', function frstCrdEl (event) {
 
 function matched (event) {
   if (event.target.classList.contains("card")){
-  let counter = document.querySelector('.moves').innerHTML;
-  if (counter % 2 !== 0){
-    // console.log("card 2 was clicked..get ready for matching");
-    if(card1 == card2){
-    console.log("Cards Match!");
-    event.target.classList.toggle("open");
-    event.target.classList.toggle("show");
-    event.target.classList.toggle("match");
-    card1El.classList.toggle("open");
-    card1El.classList.toggle("show");
-    card1El.classList.toggle("match");
-    matchCount += 1;
-    if (matchCount === 8) {
-      debugger;
-      let time = document.querySelector(".timer").innerHTML;
-      if (counter >= 57) {
-        alert("Hmm, were you even trying? You only kept 1 star and your time was " + time + " seconds!");
-        document.querySelector(".timer").innerHTML = 0;
-        matchCount = 0;
-        clearInterval(constTimer);
-        constTimer = false;
-      }
-      else if (counter >= 47) {
-        alert("Oh no, you should concentrate better next time. You only kept 2 stars and your time was " + time + " seconds!");
-        document.querySelector(".timer").innerHTML = 0;
-        matchCount = 0;
-        clearInterval(constTimer);
-        constTimer = false;
-      }
-      else if (counter >= 37) {
-        alert("Not bad, for keeping 3 stars but actively try and remember what you saw. Your time was " + time + " seconds!");
-        document.querySelector(".timer").innerHTML = 0;
-        matchCount = 0;
-        clearInterval(constTimer);
-        constTimer = false;
-      }
-      else if (counter >= 27) {
-        alert("Almost Perfect Score while keeping 4 stars. Concentrate better next time! Your time was " + time + " seconds!");
-        document.querySelector(".timer").innerHTML = 0;
-        matchCount = 0;
-        clearInterval(constTimer);
-        constTimer = false;
-      }
-      else if (counter <= 26) {
-        alert("Perfect Score! You kept all 5 stars and your time was " + time + " seconds!");
-        document.querySelector(".timer").innerHTML = 0;
-        matchCount = 0;
-        clearInterval(constTimer);
-        constTimer = false;
-      }
-    }
-    } else { //need a time function
-      setTimeout(function delayWrongAnswer() {
-        console.log("Cards don't Match!");
+    let counter = document.querySelector('.moves').innerHTML;
+    
+    if (counter % 2 !== 0){
+      if(card1 == card2){
         event.target.classList.toggle("open");
         event.target.classList.toggle("show");
+        event.target.classList.toggle("match");
         card1El.classList.toggle("open");
         card1El.classList.toggle("show");
-    }, 1000);
+        card1El.classList.toggle("match");
+        matchCount += 1;
+        
+        if (matchCount === 8) {
+          let time = document.querySelector(".timer").innerHTML;
+          
+          if (counter >= 57) {
+              alert("Hmm, were you even trying? You only kept 1 star and your time was " + time + " seconds!");
+              document.querySelector(".timer").innerHTML = 0;
+              matchCount = 0;
+              clearInterval(constTimer);
+              constTimer = false;
+          
+          } else if (counter >= 47) {
+              alert("Oh no, you should concentrate better next time. You only kept 2 stars and your time was " + time + " seconds!");
+              document.querySelector(".timer").innerHTML = 0;
+              matchCount = 0;
+              clearInterval(constTimer);
+              constTimer = false;
+          
+          } else if (counter >= 37) {
+              alert("Not bad, for keeping 3 stars but actively try and remember what you saw. Your time was " + time + " seconds!");
+              document.querySelector(".timer").innerHTML = 0;
+              matchCount = 0;
+              clearInterval(constTimer);
+              constTimer = false;
+          
+          } else if (counter >= 27) {
+              alert("Almost Perfect Score while keeping 4 stars. Concentrate better next time! Your time was " + time + " seconds!");
+              document.querySelector(".timer").innerHTML = 0;
+              matchCount = 0;
+              clearInterval(constTimer);
+              constTimer = false;
+          
+          } else if (counter <= 26) {
+              alert("Perfect Score! You kept all 5 stars and your time was " + time + " seconds!");
+              document.querySelector(".timer").innerHTML = 0;
+              matchCount = 0;
+              clearInterval(constTimer);
+              constTimer = false;
+          }
+        }
+      } else { 
+        setTimeout(function delayWrongAnswer() {
+          event.target.classList.toggle("open");
+          event.target.classList.toggle("show");
+          card1El.classList.toggle("open");
+          card1El.classList.toggle("show");
+        }, 1000);
+      }
     }
-  }
- 
   }
 }
 
 function counter (event) {
   if (event.target.classList.contains("fa") || event.target.classList.contains("card")){
-  // console.log('The icon or card was clicked!');
-  let movesEl = document.querySelector('.moves').innerHTML;
-  let movesNum = parseInt(movesEl,10) +1;
-  document.querySelector('.moves').innerHTML = movesNum;
-  const starGroup = document.querySelector('.stars');
-  if (movesNum === 27) {
-    starGroup.firstElementChild.remove();
+    let movesEl = document.querySelector('.moves').innerHTML;
+    let movesNum = parseInt(movesEl,10) +1;
+    document.querySelector('.moves').innerHTML = movesNum;
+    const starGroup = document.querySelector('.stars');
+    
+    if (movesNum === 27) {
+        starGroup.firstElementChild.remove();
+    } else if (movesNum === 37) {
+       starGroup.firstElementChild.remove();
+    } else if (movesNum === 47) {
+       starGroup.firstElementChild.remove();
+    } else if (movesNum === 57) {
+        starGroup.firstElementChild.remove();
+    }
   }
-  if (movesNum === 37) {
-    starGroup.firstElementChild.remove();
-  }
-  if (movesNum === 47) {
-    starGroup.firstElementChild.remove();
-  }
-  if (movesNum === 57) {
-    starGroup.firstElementChild.remove();
-  }
-}}
+}
 
 
 function flip (event) {
   if (event.target.classList.contains("fa")){
-  // console.log('The icon was clicked!');
-  event.target.parentElement.classList.toggle("open");
-  event.target.parentElement.classList.toggle("show");
-} else if (event.target.classList.contains("card")){
-  // console.log('The card was clicked!');
-  event.target.classList.toggle("open");
-  event.target.classList.toggle("show");
-}
+      event.target.parentElement.classList.toggle("open");
+      event.target.parentElement.classList.toggle("show");
+  } else if (event.target.classList.contains("card")){
+      event.target.classList.toggle("open");
+      event.target.classList.toggle("show");
+  }
 }
 
 
